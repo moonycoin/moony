58a4cbd45e69d24bfaf9600f13ded4db63e04497
Moony <usemoony@protonmail.com>
2025-08-10 13:29:41 -0700

    Fix responsive design: constrain content width on large screens while preserving elegant design

--- docs/.vitepress/cache/deps/vue.js
+++ docs/.vitepress/cache/deps/vue.js
@@ -168,7 +168,7 @@ import {
   withMemo,
   withModifiers,
   withScopeId
-} from "./chunk-HVR2FF6M.js";
+} from "./chunk-T36XJ43D.js";
 export {
   BaseTransition,
   BaseTransitionPropsValidators,
